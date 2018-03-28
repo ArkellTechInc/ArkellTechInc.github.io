751b1d13da3fabd5d0a3c31ad8a8b45ed1fe4c1b
kurtisauc3 <kurtistrainor@gmail.com>
2018-03-28 15:07:31 -0400

    about to start wordpressin

---
+++ atsweetlemons/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes, width=320",>
+	<meta name="description" content="High Risk Auto Insurance | Burlington.">
+	<meta name="author" content="Arkell Tech Inc">
+	<title>Sweet Lemons Insurance</title>
+	<link rel="stylesheet" href="style/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.6/css/swiper.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.6/js/swiper.min.js"></script>
+</head>
+<body>
+	<div id="container" class="containerPortrait">
+		<div id="content" class="contentPortrait">
+			<!----------------------- content goes here ----------------------->
+				<!-- Slider main container -->
+				<div class="swiperFix1 swiper-container">
+					<!-- Additional required wrapper -->
+					<div class="swiperFix2 swiper-wrapper">
+						<!-- Slides -->
+						<!-- Page 1 --------------------------------------->
+						<div id="page1" class="swiper-slide">
+							<div class="contentContainer">
+								<h1 class="pageTitle">High Risk Auto Insurance<a href="tel:1-855-447-4750"><div class="phoneBox inLine">Call Now</div></a></h1>
+								<blockquote class="buttonGradient learnMore">Get your low insurance prices back <div class="boldItalic lemons">faster!</div> Learn more about our <div class="boldItalic lemons">high risk auto insurance programs.</div>
+									<div><button onclick="toggleCallBack()" id="getStarted" class="getStarted">Request a Call Back</button></div>
+									<form class="displayNone" id="callBackForm">
+										<div class="callBackText">Let us help you find the shortest road to recovery.</div>
+										<div class="formBox">
+											<label>Name*</label>
+											<input type="text" placeholder="John Doe" name="" required></input>
+										</div>
+										<div class="formBox">
+											<label>Phone*</label>
+											<input type="tel" placeholder="123-456-7890" name="" required></input>
+										</div>
+										<div class="formBox">
+											<button type="submit">Call Me Back</button>
+										</div>
+									</form>
+								</blockquote>
+									<div id="iconContainer">
+										<button id="carelessDrivingBox" class="iconBox" onclick="toggleBox(1)">
+											<img src="images/cdIcon.png" alt="careless driving icon">
+											<div class="iconLabel">Careless Driving</div>
+										</button>
+										<button id="impairedDrivingBox" class="iconBox" onclick="toggleBox(2)">
+											<img src="images/idIcon.png" alt="impaired driving icon">
+											<div class="iconLabel">Impaired Driving</div>
+										</button><br />
+										<button id="streetRacingBox" class="iconBox" onclick="toggleBox(3)">
+											<img src="images/srIcon.png" alt="street racing icon">
+											<div class="iconLabel">Street Racing</div>
+										</button>
+										<button id="trafficTicketsBox" class="iconBox" onclick="toggleBox(4)">
+											<img src="images/ttIcon.png" alt="traffic tickets icon">
+											<div class="iconLabel">Traffic Tickets</div>
+										</button>
+										<div class="iconContent">
+											<div id="carelessDrivingContent">Hey it's more information on Careless Driving. Wow.</div>
+											<div id="impairedDrivingContent">Hey it's more information on Impaired Driving. Wow.</div>
+											<div id="streetRacingContent">Hey it's more information on Street Racing. Wow.</div>
+											<div id="trafficTicketsContent">Hey it's more information on Traffic Tickets. Wow.</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- Page 2 --------------------------------------->
+							<div id="page2" class="swiper-slide">
+								<div class="contentContainer">
+									<h1 class="pageTitle">30 Second Breakdown<a href="tel:1-855-447-4750"><div class="phoneBox inLine">Call Now</div></a></h1>
+									<iframe id="vid" src="https://www.youtube.com/embed/xKG1bKg4ksY?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+									<div class="infoBox" id="caption">
+										<div class="infoBoxContent">"A couple of driving mistakes can really sour your car insurance rates. Sweet Lemons Insurance introduced Kate to their driver therapy program..."</div>
+									</div>
+								</div>
+							</div>
+							<!-- Page 3 --------------------------------------->
+							<div id="page3" class="swiper-slide">
+								<div class="contentContainer">
+									<h1 class="pageTitle">About Sweet Lemons<a href="tel:1-855-447-4750"><div class="phoneBox inLine">Call Now</div></a></h1>
+									<div class="infoContainer">
+										<div class="darkSection">
+											<span class="darkText"><div class="boldItalic lemons">High risk auto insurance</div> is not a category any driver wants to find themselves in.</span><span class="secIcon"><img src="images/phoneIcon.png" alt="phone icon">
+										</div>
+										<div class="lightSection">
+											<span class="secIcon"><img src="images/phoneIcon.png" alt="phone icon"></span><span class="lightText">However there is light at the end of the tunnel.</span>
+										</div>
+										<div class="darkSection">
+											<span class="darkText">If you have <div class="boldItalic lemons">high risk auto insurance</div> now, call us.</span><span class="secIcon"><img src="images/phoneIcon.png" alt="phone icon">
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- Page 3 --------------------------------------->
+							<div id="page4" class="swiper-slide">
+								<div class="contentContainer">
+									<h1 class="pageTitle">Sweet Lemons<a href="tel:1-855-447-4750"><div class="phoneBox inLine">Call Now</div></a></h1>
+									<div class="infoBoxLine">
+										<div class="infoBox">
+											<div class="infoBoxTitle">High Risk</div>
+											<div class="infoBoxContent">Through our <div class="boldItalic lemons">Sweet Lemons Insurance Program</div> we offer solutions for high risk drivers.</div>
+											<div class="infoBoxHighlight">Every agent at our Burlington location is <div class="boldItalic">knowledgeable</div> and <div class="boldItalic">experienced.</div></div>
+										</div>
+									</div>
+									<div class="didYouKnowBox">
+										<div class="didYouKnow">Programs in</div>
+											<ul>
+												<li>Hamilton</li>
+												<li>Burlington</li>
+												<li>Dundas</li>
+												<li>Ancaster</li>
+												<li>Grimsby</li>
+												<li>Oakville</li>
+												<li>Mississauga</li>
+											</ul>
+									</div><br />
+									<div class="infoBox" id="caption">
+										<div class="infoBoxContent">"The only speeding you should do is on the road to recovery"<br /><br /><div class="infoBoxHighlight">-VINCE PRONESTI</div></div>
+									</div>
+								</div>
+							</div>
+							<div id="page5" class="swiper-slide">
+								<div class="contentContainer">
+									<h1 class="pageTitle">Sweet Lemons<a href="tel:1-855-447-4750"><div class="phoneBox inLine">Call Now</div></a></h1>
+									<div class="infoContainer">
+										<div class="darkSection">
+											<span class="darkText">We will customize a plan that will get your low insurance rates back <div class="boldItalic lemons">faster</div>.</span><span class="secIcon"><img src="images/phoneIcon.png" alt="phone icon"></span>
+										</div>
+										<div class="lightSection">
+											<span class="secIcon"><img src="images/phoneIcon.png" alt="phone icon"></span><span class="lightText">We will educate you on the process and your options</span>
+										</div>
+										<div class="darkSection">
+											<span class="darkText">Soon you will enjoy the benefits of a clean driving record once again.</span><span class="secIcon"><img src="images/phoneIcon.png" alt="phone icon">
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- Page 5 --------------------------------------->
+						<div id="page6" class="swiper-slide">
+							<div class="contentContainer">
+							<h1 class="pageTitle">A Product Of Youngs Insurance<a href="tel:1-855-447-4750"><div class="phoneBox inLine">Call Now</div></a></h1>
+								<a href="https://www.youngsinsurance.ca/site/home" target="_blank"><div id="youngsImg"><img src="images/youngsLogo.jpg" alt="youngs insurance logo"></div></a>
+								<div class="footerBox">
+									444 Plains Rd. E., Burlington, ON - Call 905-335-6925, Toll-Free 1-855-HI-RISK-0 for more info.
+									Providing High Risk Auto Insurance to: Toronto | Mississauga | Oakville | Hamilton
+									<a href="https://www.youngsinsuranceburlington.ca" target="_blank">Youngs Insurance Burlington © 2015-2018</a>
+								</div>
+							</div>
+						</div>
+						</div>
+						<!-- Pagination buttons -->
+						<div class="swiper-pagination"></div>
+			<!----------------------- content ends here ----------------------->
+		</div>
+		<div id="blackBox" onclick="hideMenu()"></div>
+	</div>
+	<div itemscope itemtype="http://schema.org/LocalBusiness" id="sideMenu" class="sideMenuPortrait">
+		<ul>
+			<a href="https://arkelltechinc.github.io/"><li itemprop="name" id="sideMenuTitle" class="sideMenuTitlePortrait">Sweet<div class="lemons">Lemons</div>Insurance</li></a>
+			<li id="sideMenuPhone"><span itemprop="telephone"><a href="tel:905-335-6925">905-335-6925</a></span><br /><a href="tel:1-855-447-4750">1-855-HI-RISK-0</a></li>
+			<a href="https://arkelltechinc.github.io/"><li>Home</li></a>
+			<a href=""><li>High Risk Auto Insurance <div id="dropDownArrow"></div></li></a>
+			<a href=""><li class="subMenu">Careless Driving</li></a>
+			<a href=""><li class="subMenu">Impaired Driving</li></a>
+			<a href=""><li class="subMenu">Street Racing</li></a>
+			<a href="https://arkelltechinc.github.io/traffic/"><li class="subMenu">Traffic Tickets</li></a>
+			<a href=""><li>Blog</li></a>
+			<a href="https://arkelltechinc.github.io/contact/"><li>Contact Us</li></a>
+			<a href="https://www.youngsinsurance.ca/site/home" target="_blank"><li id="menuImg"><img src="images/youngsLogo.jpg" alt="youngs insurance logo"></li></a>
+		</ul>
+	</div>
+	<div id="topMenu" class="topMenuPortrait">
+		<button id="menuButton" onclick="toggleMenu()">
+			<img src="images/menuIcon.png" alt="menu icon">
+		</button>
+		<a href="tel:1-855-447-4750">
+			<button id="phoneIcon"">
+				<img src="images/phoneIcon.png" alt="phone icon">
+			</button>
+		</a>
+		<div id="topTitle" class="topTitlePortrait">
+			<button id="title" onclick="sendHome()">Sweet<div class="lemons">Lemons</div>Insurance</button>
+		</div>
+	</div>
+	<script type="text/javascript" src="scripts/dopamine.js"></script>
+</body>
+</html>
